526a7d54ac9c6072b1944f88b4ae46a34466bf63
mohammad <malhumaidaan@gmail.com>
2023-11-04 22:42:23 +0300

    I forgot what I did but I think done

--- app.js
+++ app.js
@@ -25,5 +25,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+  console.log('The application is running on localhost:8000'); 
 });
